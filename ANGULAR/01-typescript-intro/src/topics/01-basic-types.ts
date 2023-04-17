ad651ed2e54c777d408e75573ce638f638d7d630
ritarobledo <ritarobledo@bitbucket.org>
2023-04-17 18:24:23 -0300

    subindo pro github

---
+++ ANGULAR/01-typescript-intro/src/topics/01-basic-types.ts
@@ -0,0 +1,11 @@
+const name: string = 'Strider';
+let hpPoints: number | string =100;
+const isAlive:boolean=true;
+
+hpPoints='Aloha';
+
+console.log({
+    name, hpPoints,isAlive
+});
+
+export{};
---
+++ ANGULAR/01-typescript-intro/src/topics/02-object-interface.ts
@@ -0,0 +1,7 @@
+let skills: string[] = ['Bash','Counter','Healing'];
+
+
+
+
+
+export {};
